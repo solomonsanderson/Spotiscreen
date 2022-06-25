d06512079fae748aedda4e8690fb9021b8532f90
Solomon Sanderson <solomonsanderson@outlook.com>
2022-06-25 15:33:23 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ redirect_uri = "http://localhost:8888/callback/"
 token = util.prompt_for_user_token(username= credentials.username, scope = scope,redirect_uri=  redirect_uri, client_id = credentials.client_id, client_secret= credentials.client_secret)
 sp = spotipy.Spotify(auth=token)
 
-
+prev_album_art_url = None
 while not False:
     playback = sp.current_playback()
     n_playback = pd.json_normalize(playback)
@@ -29,10 +29,14 @@ while not False:
 
     elif playing == True:
         album_art_url = n_playback['item.album.images'][0][0]["url"]
-        print(n_playback['item.album.images'][0][0]["url"])
-        response = requests.get(album_art_url)
-        file = open("album_art.png", "wb")
-        file.write(response.content)
-        file.close()
-        print("playing")
+        if album_art_url == prev_album_art_url:
+            pass
+        elif album_art_url != prev_album_art_url:
+            print(n_playback['item.album.images'][0][0]["url"])
+            response = requests.get(album_art_url)
+            file = open("album_art.png", "wb")
+            file.write(response.content)
+            file.close()
+            print("playing")
+        prev_album_art_url = album_art_url
     time.sleep(1)

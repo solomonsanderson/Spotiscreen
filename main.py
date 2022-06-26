7154784778f58aba5cd4623ac18f9f1cfa6ba016
Solomon Sanderson <solomonsanderson@outlook.com>
2022-06-26 19:55:03 +0100

    changed auth method
    changed auth method so that the token does not expire, needs testing

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from PIL import Image
 import spotipy
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
+import spotipy.oauth2 as oauth2
 import credentials
 import time
 import sys
@@ -16,42 +18,44 @@ import subprocess
 scope = "user-read-playback-state"
 redirect = "http://localhost:7777/callback"
 redirect_uri = "http://localhost:8888/callback/"
-token = util.prompt_for_user_token(username= credentials.username, scope = scope,redirect_uri=  redirect_uri, client_id = credentials.client_id, client_secret= credentials.client_secret)
-sp = spotipy.Spotify(auth=token)
-print(sys.argv)
+auth_manager = SpotifyOAuth(username= credentials.username, scope = scope,redirect_uri=redirect_uri, client_id = credentials.client_id, client_secret= credentials.client_secret)
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
 prev_album_art_url = None
-token_time = 0 
+
 while True:
-    try:
-        playback = sp.current_playback()
-        if playback == None:
-            print("paused")
-        else:
-            n_playback = pd.json_normalize(playback)
-            playing = n_playback['is_playing'][0]
-            if playing == False:
-                print("paused")
-            elif playing == True:
-                album_art_url = n_playback['item.album.images'][0][0]["url"]
-                if album_art_url == prev_album_art_url:
-                    print("Playing, No Change")
-                elif album_art_url != prev_album_art_url:
-                    print("Downloading Album Cover")
-                    print(n_playback['item.album.images'][0][0]["url"])
-                    response = requests.get(album_art_url)
-                    file = open("album_art.png", "wb")
-                    file.write(response.content)
-                    file.close()
-                    image = subprocess.Popen("fim -a album_art.png", shell = True)
-                prev_album_art_url = album_art_url
-    except:
-        print(f"An {sys.exc_info()[0]} error occured")
-    
-    token_time += 1 
-    if token_time == 1000:
-        print(token)
-        sp = spotipy.Spotify(auth=token)
-        print(sp)
+    token_info = auth_manager.get_cached_token()
+    if auth_manager.is_token_expired(token_info) == True:
+        auth_manager = SpotifyOAuth(username= credentials.username, scope = scope,redirect_uri=redirect_uri, client_id = credentials.client_id, client_secret= credentials.client_secret)
+        sp = spotipy.Spotify(auth_manager=auth_manager)
         print("token refreshed")
-        token_time = 0 
+         
+    #    print(auth_manager.get_access_token(as_dict=False))
+
+
+    playback = sp.current_playback()
+    if playback == None:
+        print("paused")
+    else:
+        n_playback = pd.json_normalize(playback)
+        playing = n_playback['is_playing'][0]
+        if playing == False:
+            print("paused")
+        elif playing == True:
+            album_art_url = n_playback['item.album.images'][0][0]["url"]
+            if album_art_url == prev_album_art_url:
+                print("Playing, No Change")
+            elif album_art_url != prev_album_art_url:
+                print("Downloading Album Cover")
+                print(n_playback['item.album.images'][0][0]["url"])
+                response = requests.get(album_art_url)
+                file = open("album_art.png", "wb")
+                file.write(response.content)
+                file.close()
+                image = subprocess.Popen("fim -a album_art.png", shell = True)
+            prev_album_art_url = album_art_url
+    # except:
+    #     print(f"An {sys.exc_info()[0]} error occured")
+    
+
     time.sleep(1)

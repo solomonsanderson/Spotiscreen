cf25994553b650d0f45f3552d18f2e3092710c96
Solomon Sanderson <solomonsanderson@outlook.com>
2022-07-03 17:18:31 +0100

    created matrix_weather.py

--- get_weather.py
+++ get_weather.py
@@ -3,24 +3,42 @@
 
 import requests
 import pandas as pd
+import geocoder
+import sys
 
 
-response = requests.get("http://api.weatherapi.com/v1/current.json?key=e3052fb6b4594c309bb234000220207&q=B29 6BP")
-# print(response.status_code)
-# print(response.json())
+def get_weather(location):
+    if location == "auto":
+        g = geocoder.ip('me')
+        print(g.latlng)
+        latlng = str(g.latlng)
+        for i in ["[", "]"," "]:
+            latlng = latlng.replace(i, "")
+        print(latlng)
+        response = requests.get("http://api.weatherapi.com/v1/current.json?key=e3052fb6b4594c309bb234000220207&q=" + str(latlng))
 
-norm_response = pd.json_normalize(response.json())
-# print(norm_response)
-current_temp = norm_response["current.temp_c"]
-current_condition = norm_response["current.condition.text"]
-# print(current_temp)
-print(norm_response["current.condition.icon"])
+    else:
+        response = requests.get("http://api.weatherapi.com/v1/current.json?key=e3052fb6b4594c309bb234000220207&q=" + str(location))
 
-icon_url = (norm_response["current.condition.icon"][0])
-# print(icon)
-print(icon_url)
-icon = requests.get("http:" + icon_url)
-file = open("weather_icon.png", "wb")
-file.write(icon.content)
-file.close()
+    # print(response.status_code)
+    print(response.json())
 
+    norm_response = pd.json_normalize(response.json())
+    # print(norm_response)
+    current_temp = norm_response["current.temp_c"]
+    current_condition = norm_response["current.condition.text"]
+    # print(current_temp)
+    # print(norm_response["current.condition.icon"])
+
+    icon_url = (norm_response["current.condition.icon"][0])
+    # print(icon)
+    # print(icon_url)
+    icon = requests.get("http:" + icon_url)
+    file = open("weather_icon.png", "wb")
+    file.write(icon.content)
+    file.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        exit(f"1 args required, {len(sys.argv)-1} given")
+    get_weather(sys.argv[1])
---
+++ matrix_weather.py
@@ -0,0 +1,7 @@
+'''Code to display weather information on the matrix'''
+
+
+import get_weather
+
+
+get_weather.get_weather("")

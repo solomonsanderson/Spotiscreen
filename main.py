af95de00712a8e2d0d2bdf4fbf1832b115002ce9
Solomon Sanderson <solomonsanderson@outlook.com>
2022-08-03 23:30:19 +0100

    weather should now be dispayed

--- main.py
+++ main.py
@@ -8,29 +8,28 @@ from spotipy.oauth2 import SpotifyOAuth
 import credentials
 import time
 from io import BytesIO
-from rgbmatrix import RGBMatrix, RGBMatrixOptions
+# from rgbmatrix import RGBMatrix, RGBMatrixOptions
 import sys
 import subprocess
 from get_weather import get_weather
 from datetime import datetime, timedelta, date
 
 scope = "user-read-playback-state"
-redirect = "http://localhost:7777/callback"
 redirect_uri = "http://localhost:8888/callback/"
 auth_manager = SpotifyOAuth(username= credentials.username, scope = scope,redirect_uri=redirect_uri, client_id = credentials.client_id, client_secret= credentials.client_secret)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
 prev_album_art_url = None
 
-# creating matrix object
-options = RGBMatrixOptions()
-options.rows = 32
-options.chain_length = 1
-options.parallel = 1
-options.hardware_mapping = "regular"
-options.limit_refresh_rate_hz = 100
-options.brightness=100
-matrix = RGBMatrix(options = options)
+# # creating matrix object
+# options = RGBMatrixOptions()
+# options.rows = 32
+# options.chain_length = 1
+# options.parallel = 1
+# options.hardware_mapping = "regular"
+# options.limit_refresh_rate_hz = 100
+# options.brightness=100
+# matrix = RGBMatrix(options = options)
 
 
 call_datetime = (datetime.now() - timedelta(hours=2))
@@ -46,6 +45,7 @@ while True:
         sp = spotipy.Spotify(auth_manager=auth_manager)
         print("token refreshed")
     #    print(auth_manager.get_access_token(as_dict=False))
+    
     playback = sp.current_playback()
     if playback == None:
         print("paused")
@@ -54,19 +54,30 @@ while True:
         playing = n_playback['is_playing'][0]
         if playing == False:
             print("paused")
-            now = datetime.now().time()
-            dt = datetime.combine(date.today(), now) - datetime.combine(date.today(), call_time)
-            interval = timedelta(seconds = 20)
-            if ( dt > interval):
-                get_weather("B296BP")
-                call_time = datetime.now().time()
+            # now = datetime.now().time()
+            # dt = datetime.combine(date.today(), now) - datetime.combine(date.today(), call_time)
+            # interval = timedelta(seconds = 20)
+            # if ( dt > interval):
+            #     get_weather("B296BP")
+            #     call_time = datetime.now().time()
+            t=0
+            weather_delay = 0
+            while t <= weather_delay:
+                print(t)
+                t = t++1
+                time.sleep(1)
+            
+            get_weather("B296BP")
+
         elif playing == True:
+            print("Playing")
+            t=0
             album_art_url = n_playback['item.album.images'][0][0]["url"]
             if prev_album_art_url != album_art_url:
                 response = requests.get(album_art_url)
                 cover = Image.open(BytesIO(response.content))
-                cover.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
-                matrix.SetImage(cover.convert("RGB"))
+                # cover.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+                # matrix.SetImage(cover.convert("RGB"))
                 prev_album_art_url = album_art_url
     # except:
     #     print(f"An {sys.exc_info()} error occured")
@@ -77,14 +88,14 @@ while True:
         #     playing = n_playback['is_playing'][0]
 
 
-    if playing == False:
-        print("paused")
-        now = datetime.now().time()
-        dt = datetime.combine(date.today(), now) - datetime.combine(date.today(), call_time)
-        interval = timedelta(seconds = 20)
-        if ( dt > interval):
-            get_weather("Austrailia")
-            call_time = datetime.now().time()
+        # if playing == False:
+        #     print("paused")
+        #     now = datetime.now().time()
+        #     dt = datetime.combine(date.today(), now) - datetime.combine(date.today(), call_time)
+        #     interval = timedelta(seconds = 20)
+        #     if ( dt > interval):
+        #         get_weather("Austrailia")
+        #         call_time = datetime.now().time()
             
             # display_image("weather_icon.png")
 

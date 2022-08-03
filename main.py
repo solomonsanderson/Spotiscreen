42c10f40a4d6e80eb7a1a6477838fd9eefd11cb8
Solomon Sanderson <solomonsanderson@outlook.com>
2022-08-03 20:48:48 +0100

    rempde try/except statement for debugging

--- main.py
+++ main.py
@@ -39,43 +39,35 @@ call_time = call_datetime.time()
 
 
 while True:
-    try:
-        token_info = auth_manager.get_cached_token()
-        if auth_manager.is_token_expired(token_info) == True:
-            auth_manager = SpotifyOAuth(username= credentials.username, scope = scope,redirect_uri=redirect_uri, client_id = credentials.client_id, client_secret= credentials.client_secret)
-            sp = spotipy.Spotify(auth_manager=auth_manager)
-            print("token refreshed")
-        #    print(auth_manager.get_access_token(as_dict=False))
-
-        playback = sp.current_playback()
-
-        if playback == None:
+    # try:
+    token_info = auth_manager.get_cached_token()
+    if auth_manager.is_token_expired(token_info) == True:
+        auth_manager = SpotifyOAuth(username= credentials.username, scope = scope,redirect_uri=redirect_uri, client_id = credentials.client_id, client_secret= credentials.client_secret)
+        sp = spotipy.Spotify(auth_manager=auth_manager)
+        print("token refreshed")
+    #    print(auth_manager.get_access_token(as_dict=False))
+    playback = sp.current_playback()
+    if playback == None:
+        print("paused")
+    else:
+        n_playback = pd.json_normalize(playback)
+        playing = n_playback['is_playing'][0]
+        if playing == False:
             print("paused")
-
-        else:
-            n_playback = pd.json_normalize(playback)
-            playing = n_playback['is_playing'][0]
-            if playing == False:
-                print("paused")
-                now = datetime.now().time()
-                dt = datetime.combine(date.today(), now) - datetime.combine(date.today(), call_time)
-                interval = timedelta(seconds = 20)
-
-                if ( dt > interval):
-                    get_weather("B296BP")
-                    call_time = datetime.now().time()
-
-
-            elif playing == True:
-                album_art_url = n_playback['item.album.images'][0][0]["url"]
-
-                if prev_album_art_url != album_art_url:
-
-                    response = requests.get(album_art_url)
-                    cover = Image.open(BytesIO(response.content))
-                    cover.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
-                    matrix.SetImage(cover.convert("RGB"))
-                    prev_album_art_url = album_art_url
+            now = datetime.now().time()
+            dt = datetime.combine(date.today(), now) - datetime.combine(date.today(), call_time)
+            interval = timedelta(seconds = 20)
+            if ( dt > interval):
+                get_weather("B296BP")
+                call_time = datetime.now().time()
+        elif playing == True:
+            album_art_url = n_playback['item.album.images'][0][0]["url"]
+            if prev_album_art_url != album_art_url:
+                response = requests.get(album_art_url)
+                cover = Image.open(BytesIO(response.content))
+                cover.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+                matrix.SetImage(cover.convert("RGB"))
+                prev_album_art_url = album_art_url
     # except:
     #     print(f"An {sys.exc_info()} error occured")
 
@@ -85,16 +77,14 @@ while True:
         #     playing = n_playback['is_playing'][0]
 
 
-        if playing == False:
-            print("paused")
-            now = datetime.now().time()
-            dt = datetime.combine(date.today(), now) - datetime.combine(date.today(), call_time)
-            interval = timedelta(seconds = 20)
-
-
-            if ( dt > interval):
-                get_weather("Austrailia")
-                call_time = datetime.now().time()
+    if playing == False:
+        print("paused")
+        now = datetime.now().time()
+        dt = datetime.combine(date.today(), now) - datetime.combine(date.today(), call_time)
+        interval = timedelta(seconds = 20)
+        if ( dt > interval):
+            get_weather("Austrailia")
+            call_time = datetime.now().time()
             
             # display_image("weather_icon.png")
 
@@ -108,8 +98,8 @@ while True:
         #         print(n_playback['item.album.images'][0][0]["url"])
         #         # display_image("album_art.png")
         #     prev_album_art_url = album_art_url
-    except:
-        print(f"An {sys.exc_info()} error occured")
+    # except:
+    #     print(f"An {sys.exc_info()} error occured")
     
 
     time.sleep(1)

19b12e6f94cc0f14ce52d4edf35e9a251a4b5b93
Solomon Sanderson <solomonsanderson@outlook.com>
2022-08-07 01:25:21 +0100

    added part to check token expiration status, token is no longer un-necisarily updated

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from spotipy.oauth2 import SpotifyOAuth
 import credentials
 import time
 from io import BytesIO
-from rgbmatrix import RGBMatrix, RGBMatrixOptions
+# from rgbmatrix import RGBMatrix, RGBMatrixOptions
 import sys
 import subprocess
 from get_weather import get_weather
@@ -20,20 +20,26 @@ redirect_uri = "http://localhost:8888/callback/"
 prev_album_art_url = None
 
 # creating matrix object
-options = RGBMatrixOptions()
-options.rows = 32
-options.chain_length = 1
-options.parallel = 1
-options.hardware_mapping = "regular"
-options.limit_refresh_rate_hz = 100
-options.brightness=100
-matrix = RGBMatrix(options = options)
+# options = RGBMatrixOptions()
+# options.rows = 32
+# options.chain_length = 1
+# options.parallel = 1
+# options.hardware_mapping = "regular"
+# options.limit_refresh_rate_hz = 100
+# options.brightness=100
+# matrix = RGBMatrix(options = options)
+
+token_expiration_status = True
 
 
 while True:
     try:
-        auth_manager = SpotifyOAuth(client_id=credentials.client_id, client_secret=credentials.client_secret, redirect_uri=redirect_uri, scope=scope)
-        sp = spotipy.Spotify(auth_manager=auth_manager)
+        if token_expiration_status == True:
+            auth_manager = SpotifyOAuth(client_id=credentials.client_id, client_secret=credentials.client_secret, redirect_uri=redirect_uri, scope=scope)
+            sp = spotipy.Spotify(auth_manager=auth_manager)
+
+        token_info = auth_manager.get_cached_token()
+        token_expiration_status = auth_manager.is_token_expired(token_info)
 
         playback = sp.current_playback()
         if playback == "None":
@@ -42,14 +48,14 @@ while True:
         else:
             n_playback = pd.json_normalize(playback)
             playing = n_playback['is_playing'][0]
-            print(playing)
+            # print(playing)
             album_art_url = n_playback['item.album.images'][0][0]["url"]
             if prev_album_art_url != album_art_url:
                 print("Updating Image")
                 response = requests.get(album_art_url)
                 cover = Image.open(BytesIO(response.content))
-                cover.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
-                matrix.SetImage(cover.convert("RGB"))
+                # cover.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+                # matrix.SetImage(cover.convert("RGB"))
                 prev_album_art_url = album_art_url
 
     except Exception as e:

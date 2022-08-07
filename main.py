2ea93626fd190c16f11c69d4087b7f81421e33b1
Solomon Sanderson <solomonsanderson@outlook.com>
2022-08-07 01:30:25 +0100

    forgot to uncomment matrix module before, added code so that these parts dont run on windows

--- main.py
+++ main.py
@@ -8,26 +8,31 @@ from spotipy.oauth2 import SpotifyOAuth
 import credentials
 import time
 from io import BytesIO
-# from rgbmatrix import RGBMatrix, RGBMatrixOptions
+import platform
 import sys
 import subprocess
 from get_weather import get_weather
 from datetime import datetime, timedelta, date
 
+# print(platform.platform())
+if platform.platform() != "Windows-10-10.0.19044-SP0":
+    from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
+
 scope = "user-read-playback-state"
 redirect_uri = "http://localhost:8888/callback/"
 
 prev_album_art_url = None
-
-# creating matrix object
-# options = RGBMatrixOptions()
-# options.rows = 32
-# options.chain_length = 1
-# options.parallel = 1
-# options.hardware_mapping = "regular"
-# options.limit_refresh_rate_hz = 100
-# options.brightness=100
-# matrix = RGBMatrix(options = options)
+if platform.platform() != "Windows-10-10.0.19044-SP0":
+    # creating matrix object
+    options = RGBMatrixOptions()
+    options.rows = 32
+    options.chain_length = 1
+    options.parallel = 1
+    options.hardware_mapping = "regular"
+    options.limit_refresh_rate_hz = 100
+    options.brightness=100
+    matrix = RGBMatrix(options = options)
 
 token_expiration_status = True
 
@@ -48,14 +53,15 @@ while True:
         else:
             n_playback = pd.json_normalize(playback)
             playing = n_playback['is_playing'][0]
-            # print(playing)
+            print(playing)
             album_art_url = n_playback['item.album.images'][0][0]["url"]
             if prev_album_art_url != album_art_url:
                 print("Updating Image")
                 response = requests.get(album_art_url)
                 cover = Image.open(BytesIO(response.content))
-                # cover.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
-                # matrix.SetImage(cover.convert("RGB"))
+                if platform.platform() != "Windows-10-10.0.19044-SP0":
+                    cover.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+                    matrix.SetImage(cover.convert("RGB"))
                 prev_album_art_url = album_art_url
 
     except Exception as e:

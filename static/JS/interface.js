54c8954bc8c55bd4b15dd75138f68a0ac7da0073
Solomon Sanderson <solomonsanderson@outlook.com>
2023-07-16 01:10:00 +0100

    added more sections inclduing credentials section to the HTML. Needs JS doing and integrating with the python code.

--- static/JS/interface.js
+++ static/JS/interface.js
@@ -24,6 +24,30 @@ function get_settings() {
 
 }
 
+function pwdVis() {
+    var box1 = document.getElementById("sensBox1");
+    var box2 = document.getElementById("sensBox2");
+    var box3 = document.getElementById("sensBox3");
+    
+    if (box1.type === "password") {
+      box1.type = "text";
+    } else {
+      box1.type = "password";
+    }
+
+    if (box2.type === "password") {
+        box2.type = "text";
+      } else {
+        box2.type = "password";
+      }
+
+      if (box3.type === "password") {
+        box3.type = "text";
+      } else {
+        box3.type = "password";
+      }
+  }
+  
 // function get_idle_image(){
 //     console.log("get_ii");
 //     var sel_image = document.getElementById("file_select").value;
